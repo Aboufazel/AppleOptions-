3553305cbb498cdcc70ba03d7d21988c3c28eab2
FrontEnd <a.ab2521@gmail.com>
2023-05-01 08:22:11 +0330

    feat : tailwind config setup colors done

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 
 import "./Components/fonts/font-face.css"
-import "./styles/main.scss"
 import "./js/include"
 import "./js/tailwind"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#1B546B" , 
+        gold:"#EAB11F", 
+        lowHint:"rgba(255, 255, 255, 0.07)",
+        hint:"#BCBCBC"
+      }
+    },
   },
   plugins: [],
 }
